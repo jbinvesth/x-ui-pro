4f4aa769f8c173334c280996c93022a814d84e29
Ivan Razin <mozaroc@gmail.com>
2025-09-30 22:00:15 +0300

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -327,6 +327,24 @@ server {
                 proxy_pass http://127.0.0.1:${sub_port};
                 break;
         }
+	location /assets/ {
+                if (\$hack = 1) {return 404;}
+                proxy_redirect off;
+                proxy_set_header Host \$host;
+                proxy_set_header X-Real-IP \$remote_addr;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                proxy_pass http://127.0.0.1:${sub_port};
+                break;
+        }
+	location /assets {
+                if (\$hack = 1) {return 404;}
+                proxy_redirect off;
+                proxy_set_header Host \$host;
+                proxy_set_header X-Real-IP \$remote_addr;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                proxy_pass http://127.0.0.1:${sub_port};
+                break;
+        }
 	#Subscription Path (json/fragment)
         location /${json_path} {
                 if (\$hack = 1) {return 404;}
